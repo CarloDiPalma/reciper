e0af2ac499d5698502a47c2dbc1e27073104bddc
CarloDiPalma <ping.06@mail.ru>
2023-07-23 17:48:42 +0300

    Edit UserViewSet

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,26 +1,29 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from requests import Response
+from rest_framework import viewsets, status
 
 from .models import User
+from .permissions import AdminAndSuperUser
+from .serializers import UserSerializer
 
 
-# class UserViewSet(viewsets.ModelViewSet):
-#     queryset = User.objects.all()
-#     serializer_class = UserSerializer
-#     permission_classes = (AdminAndSuperUser,)
-#     pagination_class = CustomPagination
-#     filter_backends = (filters.SearchFilter,)
-#     search_fields = ('username',)
-#     lookup_field = 'username'
-#     http_method_names = ['get', 'post', 'patch', 'delete']
-#
-#     def create(self, request, **kwargs):
-#         serializer = UserSerializer(data=request.data)
-#         if serializer.is_valid():
-#             email = serializer.data.get('email')
-#             if User.objects.filter(email=email).exists():
-#                 return Response(serializer.errors,
-#                                 status=status.HTTP_400_BAD_REQUEST)
-#             user, created = User.objects.get_or_create(**serializer.data)
-#             return Response(serializer.data, status=status.HTTP_201_CREATED)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = (AdminAndSuperUser,)
+    # pagination_class = CustomPagination
+    # filter_backends = (filters.SearchFilter,)
+    search_fields = ('username',)
+    lookup_field = 'username'
+    http_method_names = ['get', 'post']
+
+    def create(self, request, **kwargs):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            email = serializer.data.get('email')
+            if User.objects.filter(email=email).exists():
+                return Response(serializer.errors,
+                                status=status.HTTP_400_BAD_REQUEST)
+            user, created = User.objects.get_or_create(**serializer.data)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
