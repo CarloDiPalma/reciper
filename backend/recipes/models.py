dc6643855734d9d16884f5c407c516ef6ec34177
CarloDiPalma <ping.06@mail.ru>
2023-08-05 11:03:34 +0300

    Add ordering to Recipe model

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -82,6 +82,7 @@ class Recipe(models.Model):
     class Meta:
         verbose_name = 'Рецепт'
         verbose_name_plural = 'рецепты'
+        ordering = ('-id',)
 
     def __str__(self):
         return self.name
