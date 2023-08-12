ad48ae512c80e8e210e1ca6d58a4b791467bb7af
CarloDiPalma <ping.06@mail.ru>
2023-08-12 12:01:54 +0300

    Add postgres db settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -64,6 +64,20 @@ DATABASES = {
     }
 }
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': os.getenv(
+#             'DB_ENGINE',
+#             default="django.db.backends.postgresql"
+#         ),
+#         'NAME': os.getenv('DB_NAME', default="postgres"),
+#         'USER': os.getenv('POSTGRES_USER', default="postgres"),
+#         'PASSWORD': os.getenv('POSTGRES_PASSWORD', default="postgres"),
+#         'HOST': os.getenv('DB_HOST', default="127.0.0.1"),
+#         'PORT': os.getenv('DB_PORT', default=5432)
+#     }
+# }
+
 AUTH_PASSWORD_VALIDATORS = [
     {
         "NAME": "django.contrib.auth.password_validation"
--- backend/users/permissions.py
+++ backend/users/permissions.py
@@ -5,10 +5,8 @@ class AdminAndSuperUser(permissions.BasePermission):
     """Админ или суперюзер"""
 
     def has_permission(self, request, view):
-        if request.user.is_anonymous:
-            return False
-
-        return request.user.is_admin or request.user.is_superuser
+        return (request.user.is_admin or request.user.is_superuser
+                or not request.user.is_anonymous)
 
     def has_object_permission(self, request, view, obj):
         return request.user.is_superuser or request.user.is_admin
