b56b439a05dd1545d3d71f5e4a1eae2f04396e24
CarloDiPalma <ping.06@mail.ru>
2023-08-12 14:41:58 +0300

    Add media url to debug mode

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import TemplateView
@@ -7,5 +9,13 @@ urlpatterns = [
     path("api/", include("recipes.urls")),
     path("admin/", admin.site.urls),
     path("api-auth/", include("rest_framework.urls")),
-    path("redoc/", TemplateView.as_view(template_name="redoc.html"), name="redoc"),
+    path(
+        "redoc/",
+        TemplateView.as_view(template_name="redoc.html"),
+        name="redoc"
+    ),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
