0e07d9ee1eb49b990e4d2fac66dffd21803f724d
CarloDiPalma <ping.06@mail.ru>
2023-08-14 14:03:20 +0300

    Add static_root to settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -111,6 +111,7 @@ USE_TZ = True
 STATIC_URL = "/static/"
 
 STATICFILES_DIRS = ((BASE_DIR / "static/"),)
+STATIC_ROOT = BASE_DIR / 'collected_static'
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
